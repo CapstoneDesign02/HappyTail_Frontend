0b3ac1518ff2422f741cf29f88c55a1888f31723
eunseo <jangeun4097@naver.com>
2025-02-05 15:03:11 +0900

    [setting] next.js pwa tailwind 세팅 완료

---
+++ happytail/next.config.ts
@@ -0,0 +1,15 @@
+// next.config.ts
+import { NextConfig } from "next";
+import withPWA from "next-pwa";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  // 다른 Next.js 설정들...
+};
+
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development", // 개발 환경에서는 PWA 기능 비활성화
+})(nextConfig);
