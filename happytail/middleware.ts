5b4cd7a731b456fe4fafeabe80d0bf120a0011a3
eunseo <jangeun4097@naver.com>
2025-04-03 05:04:02 +0900

    ㅔㅈㅁ

--- happytail/middleware.ts
+++ happytail/middleware.ts
@@ -1,24 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get("token")?.value ?? null;
+  const token = req.cookies.get("token")?.value;
+  console.log("checkAuth: " + token);
 
-  // 정적 파일 요청 및 특정 경로 예외 처리
-  if (
-    req.nextUrl.pathname.startsWith("/_next/") ||
-    req.nextUrl.pathname.startsWith("/static/") ||
-    req.nextUrl.pathname.endsWith(".js") ||
-    req.nextUrl.pathname.endsWith(".json") ||
-    req.nextUrl.pathname.endsWith(".ico") ||
-    req.nextUrl.pathname.startsWith("/info") ||
-    req.nextUrl.pathname.startsWith("/login/callback") ||
-    req.nextUrl.pathname.startsWith("/join") ||
-    req.nextUrl.pathname.startsWith("/img")
-  ) {
-    return NextResponse.next();
-  }
-
-  //토큰이 없으면 "/info"로 리다이렉트
+  // 토큰이 없으면 "/webpage"로 리다이렉트
   if (!token) {
     return NextResponse.redirect(new URL("/info", req.url));
   }
@@ -26,7 +12,7 @@ export function middleware(req: NextRequest) {
   return NextResponse.next();
 }
 
-// 모든 페이지 보호, 정적 파일 및 예외 처리된 경로 제외
+// 모든 페이지를 보호
 export const config = {
-  matcher: ["/:path*"],
+  matcher: ["/((?!info).*)"], // "/webpage"를 제외한 모든 경로 보호
 };
