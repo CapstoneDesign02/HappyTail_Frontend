48fe1b399a36405458029f576228f57f72bb62c4
eunseo <jangeun4097@naver.com>
2025-04-08 15:31:49 +0900

    서비스 워커 캐시 최소한으로 잡게 수정

--- happytail/public/sw.js
+++ happytail/public/sw.js
@@ -1,42 +1,12 @@
-const CACHE_NAME = "next-pwa-cache-v1";
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache
-        .addAll([
-          "/", // 메인 페이지
-          "/favicon.ico",
-          "/manifest.json",
-          "/img/logo-10.png",
-        ])
-        .catch((error) =>
-          console.warn("⚠️ 일부 리소스를 캐싱할 수 없음:", error)
-        );
-    })
-  );
-});
-
-// 방문한 모든 페이지 캐싱
 self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return; // POST 요청 제외
+  if (event.request.method !== "GET") return;
 
   event.respondWith(
-    caches
-      .match(event.request)
-      .then((cachedResponse) => {
-        return (
-          cachedResponse ||
-          fetch(event.request).then((networkResponse) => {
-            return caches.open(CACHE_NAME).then((cache) => {
-              cache.put(event.request, networkResponse.clone()); // 캐싱
-              return networkResponse;
-            });
-          })
-        );
-      })
-      .catch(() => {
-        return caches.match("/"); // 네트워크 오류 시 홈 화면 제공
-      })
+    fetch(event.request).catch(() => {
+      // 네트워크 요청 실패 시 캐시에서 대체 (예: 홈 화면)
+      return caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || caches.match("/");
+      });
+    })
   );
 });
